1ac5301f3824fb922dc35d1aa4e2b84c69494908
eiericksilva <ericksilvaredes@gmail.com>
2023-04-07 09:07:40 -0300

    melhorando autenticação

--- backend/index.js
+++ backend/index.js
@@ -2,28 +2,70 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import cors from "cors";
 
+const getUser = (email) => {
+  const users = [
+    {
+      email: "x@x.com",
+      senha: "123",
+      role: "admin",
+    },
+    {
+      email: "y@y.com",
+      senha: "123",
+      role: "customer",
+    },
+  ];
+
+  return users.find((user) => user.email === email);
+};
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const credentials = {
-  email: "e@e.com",
-  senha: "123",
+const isAuthenticated = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, key);
+    next();
+  } catch (e) {
+    return res.status(401).json({ error: "Not authorized" });
+  }
 };
 
 const key = "qwertyuiopasdfghjklç789456";
 
+app.get("/users", isAuthenticated, (req, res) => {
+  return res.status(200).json([
+    {
+      email: "e@e.com",
+    },
+  ]);
+});
+
 app.post("/auth", (req, res) => {
   const { email, senha } = req.body;
 
-  if (email === credentials.email && senha === credentials.senha) {
+  console.log(senha);
+
+  const user = getUser(email);
+
+  if (senha === user.senha) {
+    if (user.role !== "admin") {
+      return res.status(403).send({
+        error: "User not authorized",
+      });
+    }
     return res.status(200).json({
       token: jwt.sign(
         {
           email,
           role: "admin",
         },
-        key
+        key,
+        {
+          expiresIn: "24h",
+        }
       ),
     });
   }
@@ -31,5 +73,4 @@ app.post("/auth", (req, res) => {
     error: "dados conferem",
   });
 });
-
 app.listen(3000, () => console.log("Running on port 3000!"));
--- frontend/src/components/PrivateRoute/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,11 +1,16 @@
 import Cookies from "js-cookie";
 import { Navigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
 const PrivateRoute = ({ children }) => {
   const token = Cookies.get("token");
 
   if (token) {
-    return children;
+    const payload = jwtDecode(token);
+    if (Date.now() < payload.exp * 1000) {
+      return children;
+    }
+    Cookies.remove("token");
   }
   return <Navigate to="/" />;
 };
